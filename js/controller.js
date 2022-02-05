6029a3ecc4450b277016200df1f221b097fe2c10
engineerturph <benimadimturabi@gmail.com>
2022-02-06 00:56:35 +0300

    Initial commit

---
+++ js/controller.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//body uzunlugu 900 genisligi 1663 4x3 bi uygulama yapcam genisligi 1200
+//ilk htmlyi yaz sonradan designi ekle
+//User stories
+//As a user i want a configurable pomodoro app,so that i can change anything as i want
+//svg iconset kullan
